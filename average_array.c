64bcbff44d599a57db2452e75ff1eba8b84192e3
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-23 20:36:02 +0100

    correction

--- average_array.c
+++ average_array.c
@@ -1,16 +1,20 @@
+// average_array.c
 #include <stdio.h>
 
 int main() {
     int n, somme = 0;
     scanf("%d", &n);
 
-    int tab[n];
+    int tab[n]; // Déclaration du tableau
     for(int i = 0; i < n; i++) {
         scanf("%d", &tab[i]);
-        somme += tab[i]; // on cumule la somme
+        somme += tab[i]; // On additionne les valeurs
     }
-    
+
+    // Calcul de la moyenne
     float moyenne = (float)somme / n;
+
+    // Affichage du résultat avec 2 décimales
     printf("La moyenne est : %.2f\n", moyenne);
     return 0;
 }
