ecd47518b752dde4b3faf25f573035c60c52060e
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-23 20:14:13 +0100

    second commit

---
+++ average_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, somme = 0;
+    scanf("%d", &n);
+
+    int tab[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &tab[i]);
+        somme += tab[i]; // on cumule la somme
+    }
+
+    float moyenne = (float)somme / n;
+    printf("La moyenne est : %.2f\n", moyenne);
+    return 0;
+}
